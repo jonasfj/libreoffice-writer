eb3b1553a61c3f8bdfebb0b15db8ead120a398db
Frank Meies <fme@openoffice.org>
2002-12-03 10:42:50 +0000

    #105886# Calculation whether portion is BidiPortion changes

--- sw/source/core/txtnode/fntcache.cxx
+++ sw/source/core/txtnode/fntcache.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: fntcache.cxx,v $
  *
- *  $Revision: 1.62 $
+ *  $Revision: 1.63 $
  *
- *  last change: $Author: fme $ $Date: 2002-11-15 14:34:07 $
+ *  last change: $Author: fme $ $Date: 2002-12-03 11:42:50 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -747,6 +747,7 @@ BYTE lcl_WhichPunctuation( xub_Unicode cChar )
 void SwFntObj::DrawText( SwDrawTextInfo &rInf )
 {
 
+
 static sal_Char __READONLY_DATA sDoubleSpace[] = "  ";
     BOOL bPrt = OUTDEV_PRINTER == rInf.GetOut().GetOutDevType();
     Font* pTmpFont = bPrt ? pPrtFont : GetScrFont();
@@ -766,7 +767,8 @@ static sal_Char __READONLY_DATA sDoubleSpace[] = "  ";
     const BOOL bSwitchL2R = rInf.GetFrm() && rInf.GetFrm()->IsRightToLeft() &&
                             ! rInf.IsIgnoreFrmRTL();
     const ULONG nMode = rInf.GetpOut()->GetLayoutMode();
-    const BOOL bBidiPor = ( bSwitchL2R == ( TEXT_LAYOUT_BIDI_STRONG == nMode ) );
+    const BOOL bBidiPor = ( bSwitchL2R !=
+                            ( 0 != ( TEXT_LAYOUT_BIDI_RTL & nMode ) ) );
 
     // be sure to have the correct layout mode at the printer
     if ( pPrinter )
@@ -2231,7 +2233,7 @@ sal_Bool SwDrawTextInfo::ApplyAutoColor( Font* pFnt )
         // UnderLineColor has to be changed if:
         // 1. IsAlwaysAutoColor is set
 
-        bChgUnderColor = ! bPrt && GetShell() && 
+        bChgUnderColor = ! bPrt && GetShell() &&
                 GetShell()->GetAccessibilityOptions()->IsAlwaysAutoColor();
 
         bChgFntColor = COL_AUTO == rFnt.GetColor().GetColor() || bChgUnderColor;
