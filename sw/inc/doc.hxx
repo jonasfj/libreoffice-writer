319cb687cbbc5b7a2097b6688547e0f0dcb944d4
Vladimir Glazounov <vg@openoffice.org>
2003-04-01 14:57:58 +0000

    INTEGRATION: CWS sw008 (1.38.114); FILE MERGED 2003/03/13 12:40:51 mib 1.38.114.2: #106110#: Format documents if they contain size-unadodpted OLEs after a prt change, prt handling optimized 2003/03/07 11:13:04 oj 1.38.114.1: #97593# change BaseClass member method

--- sw/inc/doc.hxx
+++ sw/inc/doc.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: doc.hxx,v $
  *
- *  $Revision: 1.40 $
+ *  $Revision: 1.41 $
  *
- *  last change: $Author: vg $ $Date: 2003-04-01 15:04:19 $
+ *  last change: $Author: vg $ $Date: 2003-04-01 15:57:58 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -1903,7 +1903,7 @@ inline sal_Bool SwNodes::IsDocNodes() const
 }
 
 inline void SwDoc::SetOLEPrtNotifyPending( sal_Bool bSet )
-{
+{ 
     bOLEPrtNotifyPending = bSet;
     if( !bSet )
         bAllOLENotify = sal_False;
