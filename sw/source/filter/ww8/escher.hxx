a5107ccacb463de2c2b7e1f55ca0bc0c8a70651d
Kurt Zenker <kz@openoffice.org>
2005-01-21 09:42:55 +0000

    INTEGRATION: CWS swqbugfixes12 (1.10.364); FILE MERGED 2005/01/07 15:42:52 od 1.10.364.1: #i30669# - methods <WinwordAnchoring::SetAnchoring(..)> and 	   <PlcDrawObj::WritePlc(..)> - convert Writer object positions, 	   if needed and possible.

--- sw/source/filter/ww8/escher.hxx
+++ sw/source/filter/ww8/escher.hxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: escher.hxx,v $
  *
- *  $Revision: 1.10 $
+ *  $Revision: 1.11 $
  *
- *  last change: $Author: obo $ $Date: 2004-04-27 14:10:15 $
+ *  last change: $Author: kz $ $Date: 2005-01-21 10:42:55 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -70,18 +70,56 @@
 
 const sal_uInt32 nInlineHack = 0x00010001;
 class SwFrmFmt;
+// --> OD 2005-01-06 #i30669#
+class SwFmtHoriOrient;
+class SwFmtVertOrient;
+// <--
 
 class WinwordAnchoring : public EscherExClientRecord_Base
 {
 public:
     void WriteData(EscherEx& rEx) const;
     void SetAnchoring(const SwFrmFmt& rFmt);
+
+    /** method to perform conversion of positioning attributes with the help
+        of corresponding layout information
+
+        OD 2005-01-06 #i30669#
+        Because most of the Writer object positions doesn't correspond to the
+        object positions in WW8, this method converts the positioning
+        attributes. For this conversion the corresponding layout information
+        is needed. If no layout information exists - e.g. no layout exists - no
+        conversion is performed.
+        No conversion is performed for as-character anchored objects. Whose
+        object positions are already treated special in method <WriteData(..)>.
+        Usage of method: Used by method <SetAnchoring(..)>, nothing else
+
+        @author OD
+
+        @param _iorHoriOri
+        input/output parameter - containing the current horizontal position
+        attributes, which are converted by this method.
+
+        @param _iorVertOri
+        input/output parameter - containing the current vertical position
+        attributes, which are converted by this method.
+
+        @param _rFrmFmt
+        input parameter - frame format of the anchored object
+
+        @return boolean, indicating, if a conversion has been performed.
+    */
+    static bool ConvertPosition( SwFmtHoriOrient& _iorHoriOri,
+                                 SwFmtVertOrient& _iorVertOri,
+                                 const SwFrmFmt& _rFrmFmt );
+
 private:
     bool mbInline;
     sal_uInt32 mnXAlign;
     sal_uInt32 mnYAlign;
     sal_uInt32 mnXRelTo;
     sal_uInt32 mnYRelTo;
+
 };
 
 class SwBasicEscherEx : public EscherEx
@@ -96,9 +134,9 @@ protected:
 
     virtual INT32 WriteFlyFrameAttr(const SwFrmFmt& rFmt, MSO_SPT eShapeType,
         EscherPropertyContainer& rPropOpt);
-    void WriteBrushAttr(const SvxBrushItem &rBrush, 
+    void WriteBrushAttr(const SvxBrushItem &rBrush,
         EscherPropertyContainer& rPropOpt);
-    void WriteOLEPicture(EscherPropertyContainer &rPropOpt, 
+    void WriteOLEPicture(EscherPropertyContainer &rPropOpt,
         sal_uInt32 nShapeFlags, const Graphic &rGraphic, const SdrObject &rObj,
         sal_uInt32 nShapeId);
     void WriteGrfAttr(const SwNoTxtNode& rNd,EscherPropertyContainer& rPropOpt);
@@ -111,7 +149,7 @@ protected:
     SvStream* QueryPicStream();
 
     virtual void SetPicId(const SdrObject &, UINT32, EscherPropertyContainer &);
-    SdrLayerID GetInvisibleHellId() const 
+    SdrLayerID GetInvisibleHellId() const
         {return rWrt.pDoc->GetInvisibleHellId();}
 public:
     SwBasicEscherEx(SvStream* pStrm, SwWW8Writer& rWrt, UINT32 nDrawings = 1);
@@ -135,7 +173,7 @@ private:
     WinwordAnchoring aWinwordAnchoring;
     WW8_WrPlcTxtBoxes *pTxtBxs;
 
-    UINT32 GetFlyShapeId(const SwFrmFmt& rFmt, 
+    UINT32 GetFlyShapeId(const SwFrmFmt& rFmt,
         unsigned int nHdFtIndex, DrawObjPointerVector &rPVec);
     void MakeZOrderArrAndFollowIds(std::vector<DrawObj>& rSrcArr,
         DrawObjPointerVector& rDstArr);
@@ -161,7 +199,7 @@ public:
 
     virtual void WriteFrmExtraData(const SwFrmFmt& rFmt);
 
-    EscherExHostAppData* StartShape(const com::sun::star::uno::Reference< 
+    EscherExHostAppData* StartShape(const com::sun::star::uno::Reference<
         com::sun::star::drawing::XShape > &) {return &aHostData;}
 private:
     //No copying
