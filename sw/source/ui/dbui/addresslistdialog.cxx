35eb22af7c65fdc767e5e77edd72000c3e235c85
Vladimir Glazounov <vg@openoffice.org>
2005-03-10 14:55:07 +0000

    INTEGRATION: CWS dba24 (1.4.226); FILE MERGED 2005/03/08 09:26:43 fs 1.4.226.2: RESYNC: (1.4-1.5); FILE MERGED 2005/02/21 14:27:28 oj 1.4.226.1: #i40404# don't show the biblio as data source

--- sw/source/ui/dbui/addresslistdialog.cxx
+++ sw/source/ui/dbui/addresslistdialog.cxx
@@ -2,9 +2,9 @@
  *
  *  $RCSfile: addresslistdialog.cxx,v $
  *
- *  $Revision: 1.6 $
+ *  $Revision: 1.7 $
  *
- *  last change: $Author: vg $ $Date: 2005-03-07 17:34:52 $
+ *  last change: $Author: vg $ $Date: 2005-03-10 15:55:07 $
  *
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
@@ -88,6 +88,9 @@
 #endif
 #ifndef _DBMGR_HXX
 #include <dbmgr.hxx>
+#endif
+#ifndef _DBCONFIG_HXX
+#include <dbconfig.hxx>
 #endif
 #ifndef _UNOTOOLS_TEMPFILE_HXX
 #include <unotools/tempfile.hxx>
@@ -200,7 +203,7 @@ struct AddressUserData_Impl
     ::rtl::OUString                    sURL; // data is editable
     sal_Int32                          nCommandType;
     sal_Int32                          nTableAndQueryCount;
-    AddressUserData_Impl() : 
+    AddressUserData_Impl() :
         nCommandType(0),
         nTableAndQueryCount(-1)
         {}
@@ -209,7 +212,7 @@ struct AddressUserData_Impl
 {
     ::rtl::OUString sURL;
     if(xSourceProperties.is())
-    {        
+    {
         rtl::OUString sDBURL;
         xSourceProperties->getPropertyValue(C2U("URL")) >>= sDBURL;
         if(String(sDBURL).SearchAscii("sdbc:flat:") == 0)
@@ -234,16 +237,16 @@ struct AddressUserData_Impl
                         aInfo[nInfo].Value >>= sExtension;
                     else if(aInfo[nInfo].Name == C2U("CharSet"))
                         aInfo[nInfo].Value >>= sCharSet;
-                } 
+                }
                 if(!sCharSet.compareToAscii( cUTF8 ))
-                {        
+                {
                     sURL = String(sDBURL).Copy( 10 );
                     sURL += C2U("/");
                     sURL += aFilters[0];
                     sURL += C2U(".");
                     sURL += sExtension;
-                }            
-            }            
+                }
+            }
         }
     }
     return sURL;
@@ -284,7 +287,7 @@ SwAddressListDialog::SwAddressListDialog(SwMailMergeAddressBlockPage* pParent) :
     m_aCreateListPB.SetClickHdl( LINK( this, SwAddressListDialog,CreateHdl_Impl ));
     m_aEditPB.SetClickHdl(LINK( this, SwAddressListDialog, EditHdl_Impl));
     m_aTablePB.SetClickHdl(LINK( this, SwAddressListDialog, TableSelectHdl_Impl));
-    
+
     Size aLBSize(m_aListLB.GetSizePixel());
     m_aListHB.SetSizePixel(aLBSize);
     Size aHeadSize(m_aListHB.CalcWindowSizePixel());
@@ -295,7 +298,7 @@ SwAddressListDialog::SwAddressListDialog(SwMailMergeAddressBlockPage* pParent) :
     aLBPos.Y() += aHeadSize.Height();
     aLBSize.Height() -= aHeadSize.Height();
     m_aListLB.SetPosSizePixel(aLBPos, aLBSize);
-    
+
     Size aSz(m_aListHB.GetOutputSizePixel());
     m_aListHB.InsertItem( ITEMID_NAME, m_sName,
                             aSz.Width()/2,
@@ -321,16 +324,20 @@ SwAddressListDialog::SwAddressListDialog(SwMailMergeAddressBlockPage* pParent) :
     }
     SwMailMergeConfigItem& rConfigItem = m_pAddressPage->GetWizard()->GetConfigItem();
     const SwDBData& rCurrentData = rConfigItem.GetCurrentDBData();
-                
+
     DBG_ASSERT(m_xDBContext.is(), "service 'com.sun.star.sdb.DatabaseContext' not found!")
     sal_Bool bEnableEdit = sal_False;
     sal_Bool bEnableOK = sal_True;
     if(m_xDBContext.is())
     {
+        SwDBConfig aDb;
+        ::rtl::OUString sBibliography = aDb.GetBibliographySource().sDataSource;
         uno::Sequence< ::rtl::OUString> aNames = m_xDBContext->getElementNames();
         const ::rtl::OUString* pNames = aNames.getConstArray();
         for(sal_Int32 nName = 0; nName < aNames.getLength(); ++nName)
-        {        
+        {
+            if ( pNames[nName] == sBibliography )
+                continue;
             SvLBoxEntry* pEntry = m_aListLB.InsertEntry(pNames[nName]);
             AddressUserData_Impl* pUserData = new AddressUserData_Impl();
             pEntry->SetUserData(pUserData);
@@ -350,7 +357,7 @@ SwAddressListDialog::SwAddressListDialog(SwMailMergeAddressBlockPage* pParent) :
                 {
                     m_xDBContext->getByName(pNames[nName]) >>= xSourceProperties;
                     pUserData->sURL = lcl_getFlatURL( xSourceProperties );
-                    bEnableEdit = pUserData->sURL.getLength() > 0 && 
+                    bEnableEdit = pUserData->sURL.getLength() > 0 &&
                         !SWUnoHelper::UCB_IsReadOnlyFileName( pUserData->sURL );
                 }
                 catch(const uno::Exception& rEx)
@@ -359,7 +366,7 @@ SwAddressListDialog::SwAddressListDialog(SwMailMergeAddressBlockPage* pParent) :
                     bEnableOK = sal_False;
                 }
                 m_aDBData = rCurrentData;
-            }            
+            }
         }
     }
     m_aOK.Enable(m_aListLB.GetEntryCount()>0 && bEnableOK);
@@ -389,29 +396,33 @@ IMPL_LINK(SwAddressListDialog, FilterHdl_Impl, PushButton*, pButton)
     uno::Reference< XMultiServiceFactory > xMgr( ::comphelper::getProcessServiceFactory() );
     if(pSelect && xMgr.is())
     {
+        String sCommand = m_aListLB.GetEntryText(pSelect, ITEMID_TABLE - 1);
+        if ( !sCommand.Len() )
+            return 0;
+
         AddressUserData_Impl* pUserData = static_cast<AddressUserData_Impl*>(pSelect->GetUserData());
         if(pUserData->xConnection.is() )
-        {        
+        {
             try
-            {        
+            {
                 uno::Reference<XSQLQueryComposerFactory> xQFact(pUserData->xConnection, UNO_QUERY_THROW);
                 uno::Reference<XSQLQueryComposer> xComposer = xQFact->createQueryComposer();
-        
-        
+
+
                 PropertyValue aSecond;
                 aSecond.Name = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "RowSet" ) );
-                uno::Reference<XRowSet> xRowSet( 
+                uno::Reference<XRowSet> xRowSet(
                         xMgr->createInstance(C2U("com.sun.star.sdb.RowSet")), UNO_QUERY);
                 uno::Reference<XPropertySet> xRowProperties(xRowSet, UNO_QUERY);
-                xRowProperties->setPropertyValue(C2U("DataSourceName"), 
+                xRowProperties->setPropertyValue(C2U("DataSourceName"),
                         makeAny(OUString(m_aListLB.GetEntryText(pSelect, ITEMID_NAME - 1))));
                 xRowProperties->setPropertyValue(C2U("Command"), makeAny(
-                        OUString(m_aListLB.GetEntryText(pSelect, ITEMID_TABLE - 1))));
+                        OUString()));
                 xRowProperties->setPropertyValue(C2U("CommandType"), makeAny(pUserData->nCommandType));
                 xRowProperties->setPropertyValue(C2U("ActiveConnection"), makeAny(pUserData->xConnection));
                 xRowSet->execute();
                 aSecond.Value <<= xRowSet;
-                
+
                 PropertyValue aFirst;
                 aFirst.Name = ::rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "QueryComposer" ) );
                 ::rtl::OUString sQuery;
@@ -420,7 +431,7 @@ IMPL_LINK(SwAddressListDialog, FilterHdl_Impl, PushButton*, pButton)
                 if(pUserData->sFilter.getLength())
                     xComposer->setFilter(pUserData->sFilter);
                 aFirst.Value <<= xComposer;
-                
+
                 uno::Sequence<Any> aInit(2);
                 aInit[0] <<= aFirst;
                 aInit[1] <<= aSecond;
@@ -440,7 +451,7 @@ IMPL_LINK(SwAddressListDialog, FilterHdl_Impl, PushButton*, pButton)
             {
                 DBG_ERROR("exception caught in SwAddressListDialog::FilterHdl_Impl")
                 rEx;
-            }            
+            }
         }
     }
     return 0;
@@ -452,7 +463,7 @@ IMPL_LINK(SwAddressListDialog, LoadHdl_Impl,   PushButton*, pButton)
 {
     String sNewSource = SwNewDBMgr::LoadAndRegisterDataSource();
     if(sNewSource.Len())
-    {        
+    {
         SvLBoxEntry* pNewSource = m_aListLB.InsertEntry(sNewSource);
         pNewSource->SetUserData(new AddressUserData_Impl());
         m_aListLB.Select(pNewSource);
@@ -465,17 +476,17 @@ IMPL_LINK(SwAddressListDialog, LoadHdl_Impl,   PushButton*, pButton)
 IMPL_LINK(SwAddressListDialog, CreateHdl_Impl, PushButton*, pButton)
 {
     String sURL;
-    SwCreateAddressListDialog* pDlg = 
+    SwCreateAddressListDialog* pDlg =
             new SwCreateAddressListDialog(
-                    pButton, 
-                    sURL, 
+                    pButton,
+                    sURL,
                     m_pAddressPage->GetWizard()->GetConfigItem());
     if(RET_OK == pDlg->Execute())
     {
         //register the URL a new datasource
         OUString sURL = pDlg->GetURL();
         try
-        {        
+        {
             uno::Reference<XSingleServiceFactory> xFact( m_xDBContext, UNO_QUERY);
             uno::Reference<XInterface> xNewInstance = xFact->createInstance();
             INetURLObject aURL( sURL );
@@ -527,8 +538,8 @@ IMPL_LINK(SwAddressListDialog, CreateHdl_Impl, PushButton*, pButton)
                 sTmpName = aTempFile.GetURL();
             }
             xStore->storeAsURL(sTmpName, Sequence< PropertyValue >());
-            
-            
+
+
             uno::Reference<XNamingService> xNaming(m_xDBContext, UNO_QUERY);
             xNaming->registerObject( sFind, xNewInstance );
             //now insert the new source into the ListBox
@@ -541,13 +552,13 @@ IMPL_LINK(SwAddressListDialog, CreateHdl_Impl, PushButton*, pButton)
             m_pCreatedDataSource->SetUserData(pUserData);
             m_aListLB.Select(m_pCreatedDataSource);
             m_aCreateListPB.Enable(FALSE);
-        
+
         }
         catch(Exception& rEx)
         {
             rEx;
-        }    
-    }            
+        }
+    }
     delete pDlg;
     return 0;
 }
@@ -559,15 +570,15 @@ IMPL_LINK(SwAddressListDialog, EditHdl_Impl, PushButton*, pButton)
     SvLBoxEntry* pEntry = m_aListLB.FirstSelected();
     AddressUserData_Impl* pUserData = pEntry ? static_cast<AddressUserData_Impl*>(pEntry->GetUserData()) : 0;
     if(pUserData && pUserData->sURL.getLength())
-    {        
+    {
         if(pUserData->xResultSet.is())
-        {        
+        {
             SwMailMergeConfigItem& rConfigItem = m_pAddressPage->GetWizard()->GetConfigItem();
             rConfigItem.DisposeResultSet();
         }
         uno::Reference< sdbc::XCloseable > xClose( pUserData->xConnection, uno::UNO_QUERY);
         try
-        {        
+        {
             if(xClose.is())
                 xClose->close();
             pUserData->xSource = 0;
@@ -577,19 +588,19 @@ IMPL_LINK(SwAddressListDialog, EditHdl_Impl, PushButton*, pButton)
         }
         catch(uno::Exception&)
         {
-        }            
-        SwCreateAddressListDialog* pDlg = 
+        }
+        SwCreateAddressListDialog* pDlg =
                 new SwCreateAddressListDialog(
-                        pButton, 
-                        pUserData->sURL, 
+                        pButton,
+                        pUserData->sURL,
                         m_pAddressPage->GetWizard()->GetConfigItem());
         if(RET_OK == pDlg->Execute())
         {
-        }    
+        }
         delete pDlg;
     }
     return 0;
-};                    
+};
 /*-- 19.04.2004 09:41:05---------------------------------------------------
 
   -----------------------------------------------------------------------*/
@@ -599,12 +610,12 @@ IMPL_LINK(SwAddressListDialog, ListBoxSelectHdl_Impl, SvTabListBox*, EMPTYARG)
     Application::PostUserEvent( STATIC_LINK( this, SwAddressListDialog,
                                                 StaticListBoxSelectHdl_Impl ), pSelect );
     return 0;
-}    
+}
 IMPL_STATIC_LINK(SwAddressListDialog, StaticListBoxSelectHdl_Impl, SvLBoxEntry*, pSelect)
 {
-    //prevent nested calls of the select handler 
+    //prevent nested calls of the select handler
     if(pThis->m_bInSelectHdl)
-        return 0;            
+        return 0;
     pThis->EnterWait();
     pThis->m_bInSelectHdl = true;
     AddressUserData_Impl* pUserData = 0;
@@ -612,7 +623,7 @@ IMPL_STATIC_LINK(SwAddressListDialog, StaticListBoxSelectHdl_Impl, SvLBoxEntry*,
     {
         pUserData = static_cast<AddressUserData_Impl*>(pSelect->GetUserData());
         if(pUserData->nTableAndQueryCount > 1 || pUserData->nTableAndQueryCount == -1)
-        {        
+        {
             pThis->DetectTablesAndQueries(pSelect, false);
         }
         else
@@ -623,7 +634,7 @@ IMPL_STATIC_LINK(SwAddressListDialog, StaticListBoxSelectHdl_Impl, SvLBoxEntry*,
             pThis->m_aDBData.nCommandType = pUserData->nCommandType;
         }
     }
-    pThis->m_aEditPB.Enable(pUserData && pUserData->sURL.getLength() && 
+    pThis->m_aEditPB.Enable(pUserData && pUserData->sURL.getLength() &&
                     !SWUnoHelper::UCB_IsReadOnlyFileName( pUserData->sURL ) );
     pThis->m_bInSelectHdl = false;
     pThis->LeaveWait();
@@ -637,7 +648,7 @@ IMPL_STATIC_LINK(SwAddressListDialog, StaticListBoxSelectHdl_Impl, SvLBoxEntry*,
 void SwAddressListDialog::DetectTablesAndQueries(
         SvLBoxEntry* pSelect,
         bool bWidthDialog)
-{        
+{
     try
     {
         AddressUserData_Impl* pUserData = static_cast<AddressUserData_Impl*>(pSelect->GetUserData());
@@ -674,14 +685,14 @@ void SwAddressListDialog::DetectTablesAndQueries(
             }
             pUserData->nTableAndQueryCount = nTables;
             if(nTables > 1 && bWidthDialog)
-            {        
+            {
                 //now call the table select dialog - if more than one table exists
                 SwSelectDBTableDialog* pDlg = new SwSelectDBTableDialog(this, pUserData->xConnection);
                 String sTable = m_aListLB.GetEntryText(pSelect, ITEMID_TABLE - 1);
                 if(sTable.Len())
                     pDlg->SetSelectedTable(sTable, pUserData->nCommandType == CommandType::TABLE);
                 if(RET_OK == pDlg->Execute())
-                {        
+                {
                     bool bIsTable;
                     m_aDBData.sCommand = pDlg->GetSelectedTable(bIsTable);
                     m_aDBData.nCommandType = bIsTable ? CommandType::TABLE : CommandType::QUERY;
@@ -695,14 +706,14 @@ void SwAddressListDialog::DetectTablesAndQueries(
                 {
                     m_aDBData.sCommand = aTables[0];
                     m_aDBData.nCommandType = CommandType::TABLE;
-                }            
+                }
                 else
                 {
                     m_aDBData.sCommand = aQueries[0];
-                    m_aDBData.nCommandType = CommandType::QUERY; 
-                }            
-            }            
-        }            
+                    m_aDBData.nCommandType = CommandType::QUERY;
+                }
+            }
+        }
         if ( m_aDBData.sCommand.getLength() )
         {
             uno::Reference<beans::XPropertySet> xSourceProperties;
@@ -712,14 +723,15 @@ void SwAddressListDialog::DetectTablesAndQueries(
             m_aListLB.SetEntryText(m_aDBData.sCommand, pSelect, ITEMID_TABLE - 1);
             pUserData->xColumnsSupplier = SwNewDBMgr::GetColumnSupplier(pUserData->xConnection,
                                     m_aDBData.sCommand,
-                                    m_aDBData.nCommandType == CommandType::TABLE ? 
+                                    m_aDBData.nCommandType == CommandType::TABLE ?
                                             SW_DB_SELECT_TABLE : SW_DB_SELECT_QUERY );
         }
-        m_aOK.Enable(pSelect && m_aListLB.GetEntryText(pSelect, ITEMID_TABLE - 1).Len());
-        m_aFilterPB.Enable( pUserData->xConnection.is() );
+        String sCommand = m_aListLB.GetEntryText(pSelect, ITEMID_TABLE - 1);
+        m_aOK.Enable(pSelect && sCommand.Len());
+        m_aFilterPB.Enable( pUserData->xConnection.is() && sCommand.Len() );
         m_aTablePB.Enable( pUserData->nTableAndQueryCount > 1 );
     }
-    catch(Exception& rEx) 
+    catch(Exception& rEx)
     {
         DBG_ERROR("exception caught in SwAddressListDialog::DetectTablesAndQueries")
         m_aOK.Enable( sal_False );
@@ -737,11 +749,11 @@ IMPL_LINK(SwAddressListDialog, TableSelectHdl_Impl, PushButton*, pButton)
     if(pSelect)
     {
         AddressUserData_Impl* pUserData = static_cast<AddressUserData_Impl*>(pSelect->GetUserData());
-        //only call the table select dialog if tables have not been searched for or there 
-        //are more than 1 
+        //only call the table select dialog if tables have not been searched for or there
+        //are more than 1
         String sTable = m_aListLB.GetEntryText(pSelect, ITEMID_TABLE - 1);
         if( pUserData->nTableAndQueryCount > 1 || pUserData->nTableAndQueryCount == -1)
-        {        
+        {
             DetectTablesAndQueries(pSelect, true);
         }
     }
@@ -767,7 +779,7 @@ uno::Reference< XDataSource>  SwAddressListDialog::GetSource()
     uno::Reference< XDataSource>  xRet;
     SvLBoxEntry* pSelect = m_aListLB.FirstSelected();
     if(pSelect)
-    {        
+    {
         AddressUserData_Impl* pUserData = static_cast<AddressUserData_Impl*>(pSelect->GetUserData());
         xRet = pUserData->xSource;
     }
@@ -782,7 +794,7 @@ uno::Reference< XConnection>  SwAddressListDialog::GetConnection()
     uno::Reference< XConnection> xRet;
     SvLBoxEntry* pSelect = m_aListLB.FirstSelected();
     if(pSelect)
-    {        
+    {
         AddressUserData_Impl* pUserData = static_cast<AddressUserData_Impl*>(pSelect->GetUserData());
         xRet = pUserData->xConnection;
     }
@@ -796,7 +808,7 @@ uno::Reference< XColumnsSupplier> SwAddressListDialog::GetColumnsSupplier()
     uno::Reference< XColumnsSupplier> xRet;
     SvLBoxEntry* pSelect = m_aListLB.FirstSelected();
     if(pSelect)
-    {        
+    {
         AddressUserData_Impl* pUserData = static_cast<AddressUserData_Impl*>(pSelect->GetUserData());
         xRet = pUserData->xColumnsSupplier;
     }
@@ -810,7 +822,7 @@ uno::Reference< XColumnsSupplier> SwAddressListDialog::GetColumnsSupplier()
     ::rtl::OUString sRet;
     SvLBoxEntry* pSelect = m_aListLB.FirstSelected();
     if(pSelect)
-    {        
+    {
         AddressUserData_Impl* pUserData = static_cast<AddressUserData_Impl*>(pSelect->GetUserData());
         sRet = pUserData->sFilter;
     }
