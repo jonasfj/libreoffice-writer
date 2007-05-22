db52956cd05aa16d89f525c0ff6e6f0c0877b703
Vladimir Glazounov <vg@openoffice.org>
2007-05-22 15:20:04 +0000

    INTEGRATION: CWS chart2mst3 (1.15.74); FILE MERGED 2006/12/11 15:23:21 tl 1.15.74.1: #i71244# update charts in writer

--- sw/inc/pam.hxx
+++ sw/inc/pam.hxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: pam.hxx,v $
  *
- *  $Revision: 1.15 $
+ *  $Revision: 1.16 $
  *
- *  last change: $Author: hr $ $Date: 2006-08-14 15:29:15 $
+ *  last change: $Author: vg $ $Date: 2007-05-22 16:20:04 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -269,7 +269,7 @@ public:
     // etwas geschuetztes.
     FASTBOOL HasReadonlySel( bool bFormView ) const;
 
-    FASTBOOL ContainsPosition(const SwPosition & rPos) 
+    FASTBOOL ContainsPosition(const SwPosition & rPos)
     { return *Start() <= rPos && rPos <= *End(); }
 
     DECL_FIXEDMEMPOOL_NEWDEL(SwPaM);
