f9671e72c793a480e511a7283e33468f719c8855
Oliver Bolte <obo@openoffice.org>
2008-02-26 09:45:17 +0000

    INTEGRATION: CWS swcrossref01_DEV300 (1.84.2); FILE MERGED 2007/12/03 09:24:24 od 1.84.2.3: RESYNC: (1.85-1.86); FILE MERGED 2007/10/01 14:06:07 od 1.84.2.2: RESYNC: (1.84-1.85); FILE MERGED 2007/09/28 10:24:03 od 1.84.2.1: #i81002# adjustments due to refactoring of class <SwBookmark>

--- sw/source/filter/ww8/wrtww8.cxx
+++ sw/source/filter/ww8/wrtww8.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: wrtww8.cxx,v $
  *
- *  $Revision: 1.87 $
+ *  $Revision: 1.88 $
  *
- *  last change: $Author: vg $ $Date: 2008-02-12 14:23:58 $
+ *  last change: $Author: obo $ $Date: 2008-02-26 10:45:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -314,7 +314,7 @@ static void WriteDop( SwWW8Writer& rWrt )
     // i#78951#, store the value of  unknown compatability options
     rDop.SetCompatabilityOptions( rWrt.pDoc->Getn32DummyCompatabilityOptions1());
     rDop.SetCompatabilityOptions2( rWrt.pDoc->Getn32DummyCompatabilityOptions2());
-    
+
     rDop.fNoLeading = !rWrt.pDoc->get(IDocumentSettingAccess::ADD_EXT_LEADING);
     rDop.fUsePrinterMetrics = !rWrt.pDoc->get(IDocumentSettingAccess::USE_VIRTUAL_DEVICE);
 
@@ -1437,13 +1437,13 @@ void SwWW8Writer::AppendBookmarks( const SwTxtNode& rNd,
         for( USHORT n = 0; n < aArr.Count(); ++n )
         {
             const SwBookmark& rBkmk = *(SwBookmark*)aArr[ n ];
-            const SwPosition* pPos = &rBkmk.GetPos(),
-                            * pOPos = rBkmk.GetOtherPos();
+            const SwPosition* pPos = &rBkmk.GetBookmarkPos(),
+                            * pOPos = rBkmk.GetOtherBookmarkPos();
             if( pOPos && pOPos->nNode == pPos->nNode &&
                 pOPos->nContent < pPos->nContent )
             {
                 pOPos = pPos;
-                pPos = rBkmk.GetOtherPos();
+                pPos = rBkmk.GetOtherBookmarkPos();
             }
 
             if( !pOPos || ( nNd == pPos->nNode.GetIndex() &&
