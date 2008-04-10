bb88bbd23994b68a2dbc605e1c299945412ff500
Rüdiger Timm <rt@openoffice.org>
2008-04-10 19:26:34 +0000

    INTEGRATION: CWS changefileheader (1.3.1154); FILE MERGED 2008/04/01 15:59:19 thb 1.3.1154.3: #i85898# Stripping all external header guards 2008/04/01 12:55:30 thb 1.3.1154.2: #i85898# Stripping all external header guards 2008/03/31 16:58:39 rt 1.3.1154.1: #i87441# Change license header to LPGL v3.

--- sw/source/ui/inc/sharedconnection.hxx
+++ sw/source/ui/inc/sharedconnection.hxx
@@ -1,48 +1,38 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $RCSfile: sharedconnection.hxx,v $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  $Revision: 1.3 $
+ * $RCSfile: sharedconnection.hxx,v $
+ * $Revision: 1.4 $
  *
- *  last change: $Author: rt $ $Date: 2005-10-19 11:47:55 $
+ * This file is part of OpenOffice.org.
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 
 #ifndef SW_UI_SHARED_CONNECTION_HXX
 #define SW_UI_SHARED_CONNECTION_HXX
 
-#ifndef UNOTOOLS_INC_SHAREDUNOCOMPONENT_HXX
 #include <unotools/sharedunocomponent.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_SDBC_XCONNECTION_HPP_
 #include <com/sun/star/sdbc/XConnection.hpp>
-#endif
 
 typedef ::utl::SharedUNOComponent< ::com::sun::star::sdbc::XConnection >   SharedConnection;
 
