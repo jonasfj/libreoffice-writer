f6424be927228b3a9f2e54b6184aaf3dde9d9cca
Kurt Zenker <kz@openoffice.org>
2008-05-06 14:24:21 +0000

    INTEGRATION: CWS lcwarnings2 (1.7.182); FILE MERGED 2008/04/17 12:35:12 tl 1.7.182.3: RESYNC: (1.7-1.8); FILE MERGED 2008/03/03 10:09:51 tl 1.7.182.2: #i83458# warning-free code wntmsci11 2008/02/29 14:55:47 tl 1.7.182.1: #i83458# warning free code for wntmsci11

--- sw/source/ui/inc/pgfnote.hxx
+++ sw/source/ui/inc/pgfnote.hxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: pgfnote.hxx,v $
- * $Revision: 1.8 $
+ * $Revision: 1.9 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -85,9 +85,10 @@ private:
 
     long 			lMaxHeight;
 
-    using TabPage::ActivatePage;
-    using TabPage::DeactivatePage;
-    virtual void	ActivatePage( const SfxItemSet& rSet );
+    using SfxTabPage::ActivatePage;
+    using SfxTabPage::DeactivatePage;
+    
+    virtual void    ActivatePage( const SfxItemSet& rSet );
     virtual int		DeactivatePage( SfxItemSet* pSet = 0 );
 
 };
