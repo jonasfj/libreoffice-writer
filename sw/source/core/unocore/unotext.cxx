9b80f003bbccd42794138744d3568cf41cda7568
Michael Stahl <mst@openoffice.org>
2010-01-08 17:13:52 +0100

    swunolocking1: #i105557#: fix locking for SwXTextRange:  add Impl struct, and use UnoImplPtr to lock destructor.  move all implementation details out of header, into Impl struct.  use new XUnoTunnel and XServiceInfo helpers.  clean up the implementation a little.

--- sw/source/core/unocore/unotext.cxx
+++ sw/source/core/unocore/unotext.cxx
@@ -1594,17 +1594,13 @@ uno::Reference< text::XTextContent > SwXText::convertToTextFrame(
         //bookmarks have to be removed before the referenced text node is deleted in DelFullPara
         if(pStartRange)
         {
-            ::sw::mark::IMark const * const pStartBookmark = pStartRange->GetBookmark();
-            if(pStartBookmark)
-                pDoc->getIDocumentMarkAccess()->deleteMark(pStartBookmark);
-        }   
+            pStartRange->Invalidate();
+        }
         if(pEndRange)
         {
-            ::sw::mark::IMark const * const pEndBookmark = pEndRange->GetBookmark();
-            if(pEndBookmark)
-                pDoc->getIDocumentMarkAccess()->deleteMark(pEndBookmark);
-        }   
-       
+            pEndRange->Invalidate();
+        }
+
         pDoc->StartUndo( UNDO_START, NULL );
         bool bIllegalException = false;
         bool bRuntimeException = false;
