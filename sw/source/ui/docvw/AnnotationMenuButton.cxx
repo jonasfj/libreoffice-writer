c589dbb45cbe2a5474bc7b97d5f146addf47a475
Oliver-Rainer Wittmann <od@openoffice.org>
2010-02-10 13:53:11 +0100

    sw33a11y01: #i88070# - accessible Writer comments - part 3

--- sw/source/ui/docvw/AnnotationMenuButton.cxx
+++ sw/source/ui/docvw/AnnotationMenuButton.cxx
@@ -35,6 +35,7 @@
 
 #include <annotation.hrc>
 #include <app.hrc>
+#include <access.hrc>
 
 #include <svtools/useroptions.hxx>
 
@@ -48,8 +49,8 @@
 
 namespace sw { namespace annotation {
 
-Color ColorFromAlphaColor( const UINT8 aTransparency, 
-                           const Color &aFront, 
+Color ColorFromAlphaColor( const UINT8 aTransparency,
+                           const Color &aFront,
                            const Color &aBack )
 {
     return Color((UINT8)(aFront.GetRed()    * aTransparency/(double)255 + aBack.GetRed()    * (1-aTransparency/(double)255)),
@@ -62,6 +63,10 @@ AnnotationMenuButton::AnnotationMenuButton( sw::sidebarwindows::SwSidebarWin& rS
     , mrSidebarWin( rSidebarWin )
 {
     AddEventListener( LINK( &mrSidebarWin, sw::sidebarwindows::SwSidebarWin, WindowEventListener ) );
+
+    SetAccessibleName( SW_RES( STR_ACCESS_ANNOTATION_BUTTON_NAME ) );
+    SetAccessibleDescription( SW_RES( STR_ACCESS_ANNOTATION_BUTTON_DESC ) );
+    SetQuickHelpText( GetAccessibleDescription() );
 }
 
 AnnotationMenuButton::~AnnotationMenuButton()
@@ -195,6 +200,21 @@ void AnnotationMenuButton::Paint( const Rectangle& /*rRect*/ )
                               ( Application::GetSettings().GetStyleSettings().GetHighContrastMode()
                                 ? Color( COL_WHITE )
                                 : Color( COL_BLACK ) ) );
+    }
+}
+
+void AnnotationMenuButton::KeyInput( const KeyEvent& rKeyEvt )
+{
+    const KeyCode& rKeyCode = rKeyEvt.GetKeyCode();
+    const USHORT nKey = rKeyCode.GetCode();
+    if ( nKey == KEY_TAB )
+    {
+        mrSidebarWin.ActivatePostIt();
+        mrSidebarWin.GrabFocus();
+    }
+    else
+    {
+        MenuButton::KeyInput( rKeyEvt );
     }
 }
 
